fd6b8e3ae20e103205db3cc0634611bef6aeab97
Oak <2574438417@qq.com>
2024-03-01 10:31:00 +0800

    Finish lab01.

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -3,5 +3,23 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
-    return 0;
+    if (head == NULL) {
+        return 0;
+    }
+
+    node *tortoise = head;
+    node *hare = head;
+
+    while (1) {
+        if (hare->next == NULL || hare->next->next == NULL) {
+            return 0;
+        } else {
+            hare = hare->next->next;
+        }
+        tortoise = tortoise->next;
+        if (tortoise == hare) {
+            return 1;
+        }
+    }
+    //return 0;
 }
